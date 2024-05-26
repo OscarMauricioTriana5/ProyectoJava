7a5cc78fb81f49de307d3dda52e118eafbe9b587
oscar <trianaoscar000005@gmail.com>
2024-05-26 16:37:11 -0500

    12345

--- java_proyect/src/AParcialFinal/FabricaDeTrajes.java
+++ java_proyect/src/AParcialFinal/FabricaDeTrajes.java
@@ -1,15 +1,15 @@
 package AParcialFinal;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.TreeSet;
-
-import javax.swing.text.html.Option;
 
 import java.util.Scanner;
 
 public class FabricaDeTrajes implements IfabricaDeTrajes {
 
     private ArrayList<Componentes> componentesEnAlmacen;
+
     private TreeSet<Traje> trajesEnAlmacen;
     private boolean sonRebajas = false;
 
@@ -38,8 +38,8 @@ public class FabricaDeTrajes implements IfabricaDeTrajes {
     }
 
     @Override
-    public void añadirTrajeAAlmacen(String nombre, ArrayList<Componentes> piezas) {
-        Traje traje = new Traje(nombre, piezas);
+    public void añadirTrajeAAlmacen(String nombre, ArrayList<Componentes> componentes) {
+        Traje traje = new Traje(nombre, componentes);
         trajesEnAlmacen.add(traje);
     }
 
@@ -62,7 +62,7 @@ public class FabricaDeTrajes implements IfabricaDeTrajes {
 
     @Override
     public void crearEnvio() {
-        System.out.println("Envio creado");
+
     }
 
     @Override
@@ -73,12 +73,165 @@ public class FabricaDeTrajes implements IfabricaDeTrajes {
 
     }
 
-    public void Prueba() {
+    public void menuPrueba() {
+        System.out.println("Que componentes desea crear: ");
+        System.out.println("1-. Falda");
+        System.out.println("2-. Chaqueta");
+        System.out.println("3-. Pantalon");
+        System.out.println("4-. Blusa");
+        System.out.println("0-. volver al menu principal ");
+    }
+
+    public void componenteDePrueba() {
 
         añadirComponentesAAlmacen(new Falda(1, "Falda Azul", "s", "Azul", false, 450, true));
         añadirComponentesAAlmacen(new Pantalon(23, "vaqueros", "M", "Gris", false, 280, true));
         añadirComponentesAAlmacen(new Blusa(4, "Blusa sencilla", "s", "Blanca", false, 150, false));
         añadirComponentesAAlmacen(new Chaqueta(33, "Americana", "L", "Negro", false, 230, 6));
+        añadirComponentesAAlmacen(new Falda(3, "Moda", "M", "rojo", false, 124, true));
+    }
+
+    public void crearComponenteDePrueba(Scanner pr) {
+        FabricaDeTrajes Messi = new FabricaDeTrajes();
+        int SubOption;
+        do {
+            Messi.menuPrueba();
+            SubOption = pr.nextInt();
+            pr.nextLine();
+            switch (SubOption) {
+                case 1:
+                    System.out.println("Introduce los datos de la falda :");
+                    System.out.println("ID: ");
+                    int faldaId = pr.nextInt();
+                    pr.nextLine();
+                    System.out.println("Nombre: ");
+                    String faldaNombre = pr.nextLine();
+                    System.out.println("Talla: ");
+                    String faldaTalla = pr.nextLine();
+                    System.out.println("Color: ");
+                    String faldaColor = pr.nextLine();
+                    System.out.println("Es comunitario SI/NO: ");
+                    String esComunitarioStr;
+                    boolean faldaEsComunitario;
+                    do {
+                        esComunitarioStr = pr.nextLine().toLowerCase();
+                    } while (!esComunitarioStr.equals("si") && !esComunitarioStr.equals("no"));
+                    faldaEsComunitario = esComunitarioStr.equals("si");
+                    System.out.println("Precio: ");
+                    double faldaPrecio = pr.nextDouble();
+                    System.out.println("Tiene Cremallera (si/no): ");
+                    String tieneCremmallera;
+                    boolean faldaTieneCremallera = true;
+                    do {
+                        tieneCremmallera = pr.nextLine().toLowerCase();
+                    } while (!tieneCremmallera.equals("si") && !tieneCremmallera.equals("no"));
+                    faldaEsComunitario = esComunitarioStr.equals("si");
+                    Falda falda1 = new Falda(faldaId, faldaNombre, faldaTalla, faldaColor, faldaEsComunitario,
+                            faldaPrecio,
+                            faldaTieneCremallera);
+                    añadirComponentesAAlmacen(falda1);
+                    break;
+                case 2:
+                    System.out.println("Introduce los datos de la Chaqueta :");
+                    System.out.println("ID: ");
+                    int chaquetaId = pr.nextInt();
+                    pr.nextLine();
+                    System.out.println("Nombre: ");
+                    String chaquetaNombre = pr.nextLine();
+                    System.out.println("Talla: ");
+                    String chaquetaTalla = pr.nextLine();
+                    System.out.println("Color: ");
+                    String chaquetaColor = pr.nextLine();
+                    System.out.println("Es comunitario SI/NO: ");
+                    String esComunitarioStr1;
+                    boolean chaquetaEsComunitario;
+                    do {
+                        esComunitarioStr1 = pr.nextLine().toLowerCase();
+                    } while (!esComunitarioStr1.equals("si") && !esComunitarioStr1.equals("no"));
+                    chaquetaEsComunitario = esComunitarioStr1.equals("si");
+                    System.out.println("Precio: ");
+                    double chaquetaPrecio = pr.nextDouble();
+                    System.out.println("cuantos botones tiene: ");
+                    int chaquetaBotones = pr.nextInt();
+                    Chaqueta chaqueta1 = new Chaqueta(chaquetaId, chaquetaNombre, chaquetaTalla, chaquetaColor,
+                            chaquetaEsComunitario, chaquetaPrecio, chaquetaBotones);
+                    añadirComponentesAAlmacen(chaqueta1);
+                    break;
+                case 3:
+                    System.out.println("Introduce los datos del Pantalon :");
+                    System.out.println("ID: ");
+                    int pantaId = pr.nextInt();
+                    pr.nextLine();
+                    System.out.println("Nombre: ");
+                    String pantaNombre = pr.nextLine();
+                    System.out.println("Talla: ");
+                    String pantaTalla = pr.nextLine();
+                    System.out.println("Color: ");
+                    String pantaColor = pr.nextLine();
+                    System.out.println("Es comunitario SI/NO: ");
+                    String esComunitarioStr2;
+                    boolean pantaEsComunitario;
+                    do {
+                        esComunitarioStr2 = pr.nextLine().toLowerCase();
+                    } while (!esComunitarioStr2.equals("si") && !esComunitarioStr2.equals("no"));
+                    pantaEsComunitario = esComunitarioStr2.equals("si");
+                    System.out.println("Precio: ");
+                    double pantaPrecio = pr.nextDouble();
+                    System.out.println("Tiene Cremallera (si/no): ");
+                    String tieneCremmallera1;
+                    boolean pantaTieneCremallera = true;
+                    Pantalon pantalon1 = new Pantalon(pantaId, pantaNombre, pantaTalla, pantaColor, pantaEsComunitario,
+                            pantaPrecio, pantaTieneCremallera);
+                    añadirComponentesAAlmacen(pantalon1);
+                    do {
+                        tieneCremmallera1 = pr.nextLine().toLowerCase();
+                    } while (!tieneCremmallera1.equals("si") && !tieneCremmallera1.equals("no"));
+                    pantaEsComunitario = tieneCremmallera1.equals("si");
+                    break;
+                case 4:
+                    System.out.println("Introduce los datos de la Blusa :");
+                    System.out.println("ID: ");
+                    int blusaId = pr.nextInt();
+                    pr.nextLine();
+                    System.out.println("Nombre: ");
+                    String blusaNombre = pr.nextLine();
+                    System.out.println("Talla: ");
+                    String blusaTalla = pr.nextLine();
+                    System.out.println("Color: ");
+                    String blusaColor = pr.nextLine();
+                    System.out.println("Es comunitario SI/NO: ");
+                    String esComunitarioStr3;
+                    boolean blusaEsComunitario;
+                    do {
+                        esComunitarioStr3 = pr.nextLine().toLowerCase();
+                    } while (!esComunitarioStr3.equals("si") && !esComunitarioStr3.equals("no"));
+                    blusaEsComunitario = esComunitarioStr3.equals("si");
+                    System.out.println("Precio: ");
+                    double blusaPrecio = pr.nextDouble();
+                    System.out.println("Es mangalarga (si/no): ");
+                    String esMangalarga;
+                    boolean blusaEsMagalarga;
+                    do {
+                        esMangalarga = pr.nextLine().toLowerCase();
+                    } while (!esMangalarga.equals("si") && !esMangalarga.equals("no"));
+                    blusaEsMagalarga = esMangalarga.equals("si");
+                    Blusa blusa1 = new Blusa(blusaId, blusaNombre, blusaTalla, blusaColor, blusaEsComunitario,
+                            blusaPrecio, blusaEsMagalarga);
+                    añadirComponentesAAlmacen(blusa1);
+                    break;
+                case 0:
+                    System.out.println("Regresando al menu pricipal: ");
+
+                default:
+                    System.out.println("Opción no valida");
+                    break;
+
+            }
+
+        } while (SubOption != 0);
+        {
+
+        }
     }
 
     public static void main(String[] args) {
@@ -102,7 +255,7 @@ public class FabricaDeTrajes implements IfabricaDeTrajes {
                     String talla = Leo.nextLine();
                     System.out.println("Color");
                     String color = Leo.nextLine();
-                    System.out.println("Es comunitario F/V");
+                    System.out.println("Es comunitario Si/No");
                     boolean esComunitario = Leo.nextBoolean();
                     System.out.println("Precio");
                     double precio = Leo.nextDouble();
@@ -149,7 +302,8 @@ public class FabricaDeTrajes implements IfabricaDeTrajes {
                     break;
 
                 case 9:
-                    Messi.Prueba();
+                    Messi.crearComponenteDePrueba(Leo);
+
                     break;
 
                 case 0:
